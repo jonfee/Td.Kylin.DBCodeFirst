2de046995a8cd2d06232e69ec7ff24a153076e89
袁坷 <562573053@qq.com>
2016-03-07 15:54:01 +0800

    增加开通区域表

---
+++ Td.Kylin.Entity/Area/Area_Open.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Td.Kylin.Entity.Area
+{
+    /// <summary>
+    /// 开通区域表信息
+    /// </summary>
+    [Table("Area_Open", Schema = "dbo")]
+    public class Area_Open
+    {
+        /// <summary>
+        /// 区域ID
+        /// </summary>
+        public int AreaID { get; set; }
+        /// <summary>
+        /// 地区名称
+        /// </summary>
+        [Column(TypeName = "nvarchar(15)")]
+        public string AreaName { get; set; }
+        /// <summary>
+        /// 状态
+        /// </summary>
+        public bool Status { get; set; }
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        [Column(TypeName = "datetime")]
+        public DateTime CreateTime { get; set; }
+    }
+}
--- Td.Kylin.Entity/Area/System_Area.cs
+++ Td.Kylin.Entity/Area/System_Area.cs
@@ -6,7 +6,7 @@ namespace Td.Kylin.Entity
     /// <summary>
     /// 区域表
     /// </summary>
-    [Table("System_Area", Schema ="dbo")]
+    [Table("System_Area", Schema = "dbo")]
     public class System_Area
     {
         /// <summary>
@@ -16,7 +16,7 @@ namespace Td.Kylin.Entity
         /// <summary>
         /// 地区名称
         /// </summary>
-        [Column(TypeName ="nvarchar(15)")]
+        [Column(TypeName = "nvarchar(15)")]
         public string AreaName { get; set; }
         /// <summary>
         /// 父ID（1级为0）
@@ -25,16 +25,28 @@ namespace Td.Kylin.Entity
         /// <summary>
         /// 地区级别（1为省级，2为市级，3为区、县、地级市级，4为镇，5为村、街道）
         /// </summary>
-        public int AreaLevel { get; set; }
+        public int Depth { get; set; }
+        /// <summary>
+        /// 地区级别（1为省级，2为市级，3为区、县、地级市级，4为镇，5为村、街道）
+        /// </summary>
+        [Column(TypeName = "varchar(20)")]
+        public string NameSpell { get; set; }
         /// <summary>
         /// 区域路径（如：110000|110100|110101）
         /// </summary>
         [Column(TypeName = "varchar(100)")]
-        public string AreaPath { get; set; }
+        public string Layer { get; set; }
+        /// <summary>
+        /// 是否下级
+        /// </summary>
+        public bool HasChild { get; set; }
         /// <summary>
         /// 更新时间
         /// </summary>
         [Column(TypeName = "datetime")]
         public DateTime UpdateTime { get; set; }
+
+
+   
     }
 }
