12c526d25340adf514ee00f15673ecc2ff05a44c
袁坷 <562573053@qq.com>
2016-03-16 16:37:41 +0800

    开发数据库增加商家订单经纬度字段

--- Td.Kylin.PostgreSQLCodeFirst/Migrations/20160316082532_AddColumnsMerchantOrder.Designer.cs
+++ Td.Kylin.PostgreSQLCodeFirst/Migrations/20160316082532_AddColumnsMerchantOrder.Designer.cs
@@ -8,9 +8,10 @@ using Td.Kylin.PostgreSQLCodeFirst;
 namespace Td.Kylin.PostgreSQLCodeFirst.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20160316082532_AddColumnsMerchantOrder")]
+    partial class AddColumnsMerchantOrder
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "7.0.0-rc1-16348");
