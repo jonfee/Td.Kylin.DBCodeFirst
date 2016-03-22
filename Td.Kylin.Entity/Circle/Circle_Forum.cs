7aa45a2553fc365ed1c1339fa5f51d9e6f9df4cf
伍朝辉 <337883612@qq.com>
2016-03-22 10:23:38 +0800

    移除兴趣社区圈子模型中部分属性

--- Td.Kylin.Entity/Circle/Circle_Forum.cs
+++ Td.Kylin.Entity/Circle/Circle_Forum.cs
@@ -38,67 +38,69 @@ namespace Td.Kylin.Entity
         ///</summary>
         [Column(TypeName = "varchar(50)")]
         public string Description{get;set;}
-				
-		///<summary>
-		///帖子数量
-		///</summary>
-		public int TopicCount{get;set;}
-				
-		///<summary>
-		///回帖数据
-		///</summary>
-		public int ReplyCount{get;set;}
-				
-		///<summary>
-		///今天帖子数量
-		///</summary>
-		public int TodayTopicCount{get;set;}
-				
-		///<summary>
-		///今天回帖数量
-		///</summary>
-		public int TodayReplyCount{get;set;}
-				
-		///<summary>
-		///关注度，关注人数
-		///</summary>
-		public int Attention{get;set;}
-				
-		///<summary>
-		///通过总数据，日数据等计算出来的一个热度值
-		///</summary>
-		public int Hot{get;set;}
 
-        ///<summary>
-        ///版主
-        ///</summary>
-        [Column(TypeName = "varchar(200)")]
-        public string Moderators{get;set;}
-				
-		///<summary>
-		///论坛分类排序
-		///</summary>
-		public int OrderNo{get;set;}
-				
-		///<summary>
-		///发帖类型2n次方
-		///</summary>
-		public int PostType{get;set;}
-				
-		///<summary>
-		///发帖等级限制
-		///</summary>
-		public int PostLevel{get;set;}
-				
-		///<summary>
-		///不需要审核等级限制
-		///</summary>
-		public int PassLevel{get;set;}
+        #region 待删除
+        /////<summary>
+        /////帖子数量
+        /////</summary>
+        //public int TopicCount{get;set;}
+
+        /////<summary>
+        /////回帖数据
+        /////</summary>
+        //public int ReplyCount{get;set;}
+
+        /////<summary>
+        /////今天帖子数量
+        /////</summary>
+        //public int TodayTopicCount{get;set;}
+
+        /////<summary>
+        /////今天回帖数量
+        /////</summary>
+        //public int TodayReplyCount{get;set;}
+
+        /////<summary>
+        /////关注度，关注人数
+        /////</summary>
+        //public int Attention{get;set;}
+
+        /////<summary>
+        /////通过总数据，日数据等计算出来的一个热度值
+        /////</summary>
+        //public int Hot{get;set;}
+
+        //      ///<summary>
+        //      ///版主
+        //      ///</summary>
+        //      [Column(TypeName = "varchar(200)")]
+        //      public string Moderators{get;set;}
+
+        /////<summary>
+        /////论坛分类排序
+        /////</summary>
+        //public int OrderNo{get;set;}
+
+        /////<summary>
+        /////发帖类型2n次方
+        /////</summary>
+        //public int PostType{get;set;}
+
+        /////<summary>
+        /////发帖等级限制
+        /////</summary>
+        //public int PostLevel{get;set;}
+
+        /////<summary>
+        /////不需要审核等级限制
+        /////</summary>
+        //public int PassLevel{get;set;}
+
+        #endregion
 
         ///<summary>
         ///逻辑删除时间
         ///</summary>
-        
         public DateTime? DeleteTime{get;set;}
 				
 		///<summary>
