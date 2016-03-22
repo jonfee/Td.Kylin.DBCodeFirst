1b3308963a71cee8eea4e92dbd9ed30561848bdf
伍朝辉 <337883612@qq.com>
2016-03-22 18:07:19 +0800

    代理商相关数据模型

--- Td.Kylin.Entity/Agent/Agent_AreaRelation.cs
+++ Td.Kylin.Entity/Agent/Agent_AreaRelation.cs
@@ -4,45 +4,40 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace Td.Kylin.Entity
 {
     /// <summary>
-    /// 区域运营关系表
+    /// 区域代理关系表
     /// </summary>
-    [Table("Area_OperatorRelation")]
-    public class Area_OperatorRelation : BaseEntity
+    [Table("Agent_AreaRelation")]
+    public class Agent_AreaRelation : BaseEntity
     {
         /// <summary>
-        /// 运营商ID
+        /// 代理商ID
         /// </summary>
-        public long OperatorID { get; set; }
-        /// <summary>
-        /// 区域ID
-        /// </summary>
-        public int  AreaID { get; set; }
+        public long AgentID { get; set; }
 
         /// <summary>
-        /// 运营开始时间
+        /// 代理的区域ID（一般指隶属运营商区域下的下级区县ID）
         /// </summary>
-        
+        public int  AgentAreaID { get; set; }
+
+        /// <summary>
+        /// 代理开始时间
+        /// </summary>        
         public DateTime StartTime { get; set; }
+
         /// <summary>
-        /// 运营结束时间
-        /// </summary>
-        
+        /// 代理结束时间
+        /// </summary>        
         public DateTime EndTime { get; set; }
-        /// <summary>
-        /// 合同号
-        /// </summary>
-        [Column(TypeName = "varchar(20)")]
-        public string CompactNumber { get; set; }
         
         /// <summary>
         /// 备注说明
         /// </summary>
         [Column(TypeName = "varchar(300)")]
         public string Remark { get; set; }
+
         /// <summary>
         /// 创建日期
-        /// </summary>
-        
+        /// </summary>        
         public DateTime CreateTime { get; set; }
     }
 }
