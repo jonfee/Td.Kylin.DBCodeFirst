06d5e7799a67afe7b9544e73e7f18053b631c16b
伍朝辉 <337883612@qq.com>
2016-03-23 14:28:29 +0800

    新增数据迁移

---
+++ Td.Kylin.Entity/System/Config_Shake.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Td.Kylin.Entity.System
+{
+    public class Config_Shake
+    {
+       // public 
+    }
+}
--- Td.Kylin.PostgreSQLCodeFrist/Migrations/20160323055919_addColumnForMerchantGoodsSystemCategory_Icon.Designer.cs
+++ Td.Kylin.PostgreSQLCodeFrist/Migrations/20160323055919_addColumnForMerchantGoodsSystemCategory_Icon.Designer.cs
@@ -8,9 +8,10 @@ using Td.Kylin.PostgreSQLCodeFirst;
 namespace Td.Kylin.PostgreSQLCodeFirst.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20160323055919_addColumnForMerchantGoodsSystemCategory_Icon")]
+    partial class addColumnForMerchantGoodsSystemCategory_Icon
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "7.0.0-rc1-16348");
