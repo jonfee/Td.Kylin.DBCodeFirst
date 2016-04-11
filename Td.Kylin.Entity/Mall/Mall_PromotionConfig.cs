4a6ab968d6b016c197c1296ff35279566d98a48c
袁坷 <562573053@qq.com>
2016-04-11 14:42:46 +0800

    增加促销直降条件单位和枚举值

--- Td.Kylin.Entity/Mall/Mall_PromotionConfig.cs
+++ Td.Kylin.Entity/Mall/Mall_PromotionConfig.cs
@@ -8,27 +8,31 @@ namespace Td.Kylin.Entity
     /// </summary>
     [Table("Mall_PromotionConfig")]
     public class Mall_PromotionConfig : BaseEntity
-    {		
-		///<summary>
-		///促销配置项ID
-		///</summary>
-		public long ConfigID{get;set;}
-				
-		///<summary>
-		///促销ID
-		///</summary>
-		public long PromotionID{get;set;}
-				
-		///<summary>
-		///买/满/加的参照金额
-		///</summary>
-		public decimal MinMoney{get;set;}
+    {
+        ///<summary>
+        ///促销配置项ID
+        ///</summary>
+        public long ConfigID { get; set; }
+
+        ///<summary>
+        ///促销ID
+        ///</summary>
+        public long PromotionID { get; set; }
+
+        ///<summary>
+        ///买/满/加的参照金额
+        ///</summary>
+        public decimal MinMoney { get; set; }
 
         ///<summary>
         ///优惠的具体配置内容
         ///</summary>
         [Column(TypeName = "varchar(50)")]
-        public string Value{get;set;}
-				
-	}
+        public string Value { get; set; }
+        /// <summary>
+        /// 单位
+        /// </summary>
+        public int Unit { get; set; }
+
+    }
 }
---
+++ Td.Kylin.EnumLibrary/PromotionUnitType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Td.Kylin.EnumLibrary
+{
+    /// <summary>
+    /// 促销配置单位
+    /// </summary>
+    public enum PromotionUnitType
+    {
+        /// <summary>
+        /// 固定金额
+        /// </summary>
+        Number = 1,
+        /// <summary>
+        /// 促销折扣
+        /// </summary>
+        Percentage = 2,
+    }
+}
