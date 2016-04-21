d35c35285df23c0bc92d5c0ccc4a9d3cd2296393
伍朝辉 <337883612@qq.com>
2016-04-21 19:09:56 +0800

    新增商家商品订单状态枚举

---
+++ Td.Kylin.EnumLibrary/MerchantOrderStatus.cs
@@ -0,0 +1,36 @@
+﻿using Td.ComponentModel;
+
+namespace Td.Kylin.EnumLibrary
+{
+    /// <summary>
+    /// 商家订单状态
+    /// </summary>
+    public enum MerchantOrderStatus
+    {
+        /// <summary>
+        /// 待付款
+        /// </summary>
+        [Description("待付款")]
+        WaitingPayment = 1,
+        /// <summary>
+        /// 待发货
+        /// </summary>
+        [Description("待发货")]
+        WaitingDelivery = 2,
+        /// <summary>
+        /// 待收货
+        /// </summary>
+        [Description("待收货")]
+        WaitingReceipt = 4,
+        /// <summary>
+        /// 已完成
+        /// </summary>
+        [Description("已完成")]
+        Done = 8,
+        /// <summary>
+        /// 已取消
+        /// </summary>
+        [Description("已取消")]
+        Canceled = 16
+    }
+}
