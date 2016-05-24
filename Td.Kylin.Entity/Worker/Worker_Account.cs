1e31c63bf50f1c21f66a0cf68491c4746ed996fc
袁坷 <562573053@qq.com>
2016-05-24 15:12:34 +0800

    增加字段

--- Td.Kylin.Entity/Worker/Worker_Account.cs
+++ Td.Kylin.Entity/Worker/Worker_Account.cs
@@ -3,65 +3,75 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Td.Kylin.Entity
 {
-	/// <summary>
-	/// 服务职员账户
-	/// </summary>
-	[Table("Worker_Account")]
-	public class Worker_Account : BaseEntity
-	{
-		/// <summary>
-		/// 服务员ID。
-		/// </summary>
-		public long WorkerID
-		{
-			get;
-			set;
-		}
+    /// <summary>
+    /// 服务职员账户
+    /// </summary>
+    [Table("Worker_Account")]
+    public class Worker_Account : BaseEntity
+    {
+        /// <summary>
+        /// 服务员ID。
+        /// </summary>
+        public long WorkerID
+        {
+            get;
+            set;
+        }
 
-		/// <summary>
-		/// 真实姓名。
-		/// </summary>
-		[Column(TypeName = "nvarchar(10)")]
-		public string FullName
-		{
-			get;
-			set;
-		}
+        /// <summary>
+        /// 真实姓名。
+        /// </summary>
+        [Column(TypeName = "nvarchar(10)")]
+        public string FullName
+        {
+            get;
+            set;
+        }
 
-		/// <summary>
-		/// 工作状态。
-		/// </summary>
-		public short WorkingState
-		{
-			get;
-			set;
-		}
+        /// <summary>
+        /// 工作状态。
+        /// </summary>
+        public short WorkingState
+        {
+            get;
+            set;
+        }
 
-		/// <summary>
-		/// 账户状态。
-		/// </summary>
-		public short AccountStatus
-		{
-			get;
-			set;
-		}
+        /// <summary>
+        /// 账户状态。
+        /// </summary>
+        public short AccountStatus
+        {
+            get;
+            set;
+        }
 
-		/// <summary>
-		/// 激活时间。
-		/// </summary>
-		[Column(TypeName = "datetime")]public DateTime? ActiveTime
-		{
-			get;
-			set;
-		}
+        /// <summary>
+        /// 激活时间。
+        /// </summary>
+        [Column(TypeName = "datetime")]
+        public DateTime? ActiveTime
+        {
+            get;
+            set;
+        }
 
-		/// <summary>
-		/// 激活时间。
-		/// </summary>
-		[Column(TypeName = "datetime")]public DateTime? AuditTime
-		{
-			get;
-			set;
-		}
-	}
+        /// <summary>
+        /// 激活时间。
+        /// </summary>
+        [Column(TypeName = "datetime")]
+        public DateTime? AuditTime
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 默认所属区域ID。
+        /// </summary>
+        public int DefaultAreaID
+        {
+            get;
+            set;
+        }
+    }
 }
--- Td.Kylin.SQLCodefirst/Migrations/20160524035127_addColumnLegworkUserID.Designer.cs
+++ Td.Kylin.SQLCodefirst/Migrations/20160524035127_addColumnLegworkUserID.Designer.cs
@@ -8,9 +8,10 @@ using Td.Kylin.SQLCodeFirst;
 namespace Td.Kylin.SQLCodefirst.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20160524035127_addColumnLegworkUserID")]
+    partial class addColumnLegworkUserID
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "7.0.0-rc1-16348")
