5cff03e3f3356d2676235834f075da45eb4232cb
袁坷 <562573053@qq.com>
2016-06-13 15:58:37 +0800

    此刻推荐枚举

---
+++ Td.Kylin.EnumLibrary/RecommendDataType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Td.ComponentModel;
+namespace Td.Kylin.EnumLibrary
+{
+    /// <summary>
+    /// 此刻推荐类型
+    /// </summary>
+    public enum RecommendDataType
+    {
+        /// <summary>
+        /// B2C商品推荐
+        /// </summary>
+        [Description("B2C商品")]
+        B2CProduct = 1
+    }
+}
