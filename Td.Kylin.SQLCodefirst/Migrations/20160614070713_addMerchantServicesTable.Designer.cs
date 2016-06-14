7d488dec5e262454e2f418d9d11c6a501ca4c738
袁坷 <562573053@qq.com>
2016-06-14 15:08:01 +0800

    添加商品服务表

--- Td.Kylin.SQLCodefirst/Migrations/20160614070713_addMerchantServicesTable.Designer.cs
+++ Td.Kylin.SQLCodefirst/Migrations/20160614070713_addMerchantServicesTable.Designer.cs
@@ -8,9 +8,10 @@ using Td.Kylin.SQLCodeFirst;
 namespace Td.Kylin.SQLCodefirst.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20160614070713_addMerchantServicesTable")]
+    partial class addMerchantServicesTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "7.0.0-rc1-16348")
