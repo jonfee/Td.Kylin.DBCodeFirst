61e4ae204a72ab1cd89122aa6e1751f4e30c223e
袁坷 <562573053@qq.com>
2016-06-27 15:40:49 +0800

    增加用户收货地址删除字段

---
+++ Td.Kylin.SQLCodefirst/Migrations/20160627073951_UpdateUserAddress.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Data.Entity.Migrations;
+
+namespace Td.Kylin.SQLCodefirst.Migrations
+{
+    public partial class UpdateUserAddress : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDelete",
+                table: "User_Address",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(name: "IsDelete", table: "User_Address");
+        }
+    }
+}
