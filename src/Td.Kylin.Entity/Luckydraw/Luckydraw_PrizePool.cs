8c14c959ef6e636db1341c985776e847ecb43425
wen15813895276 <313959461@qq.com>
2016-07-04 10:19:25 +0800

    抽奖业务数据升级

---
+++ src/Td.Kylin.Entity/Luckydraw/Luckydraw_PrizePool.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Td.Kylin.Entity
+{
+	/// <summary>
+	/// 奖池表
+	/// </summary>
+	[Table("Luckydraw_PrizePool")]
+	public class Luckydraw_PrizePool
+	{
+		/// <summary>
+		///     记录ID，主键
+		/// </summary>
+		public long ID
+		{
+			get;
+			set;
+		}
+
+		/// <summary>
+		///     区域ID
+		/// </summary>
+		public long AreaID
+		{
+			get;
+			set;
+		}
+
+		/// <summary>
+		///   奖品ID
+		/// </summary>
+		public long PrizeID
+		{
+			get;
+			set;
+		}
+
+		///释放时间
+		/// <summary>
+		/// </summary>
+		public DateTime ReleaseTime
+		{
+			get;
+			set;
+		}
+
+		/// <summary>
+		///     剩余数
+		/// </summary>
+		public int Balance
+		{
+			get;
+			set;
+		}
+	}
+}
---
+++ src/Td.Kylin.EnumLibrary/Luckydraw/Luckydraw_TicketGivingRules.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Td.ComponentModel;
+
+namespace Td.Kylin.EnumLibrary
+{
+    public enum Luckydraw_TicketGivingRules
+    {
+		/// <summary>
+		/// 实付金额满XX元
+		/// </summary>
+		[Description("实付金额满XX元")]
+		[Alias("{ Min : {0} }")]
+		PaymentAmount =1,
+	}
+}
