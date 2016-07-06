a7d74579e77838c9009f7550eabf0d857a2c5e00
伍朝辉 <337883612@qq.com>
2016-07-06 18:29:57 +0800

    add PrizeID

---
+++ src/Td.Kylin.SQLCodefirst/Migrations/20160706102710_addColumnForLuckydrawLotteryRecord_PrizeID.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Td.Kylin.SQLCodefirst.Migrations
+{
+    public partial class addColumnForLuckydrawLotteryRecord_PrizeID : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "PrizeID",
+                table: "Luckydraw_LotteryRecord",
+                nullable: false,
+                defaultValue: 0L);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PrizeID",
+                table: "Luckydraw_LotteryRecord");
+        }
+    }
+}
