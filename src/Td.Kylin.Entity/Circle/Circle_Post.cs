dadf5578bb9c132b090619c9b57170213b6be89a
yk <yk@DESKTOP-97CR00P>
2016-09-09 17:27:26 +0800

    社区调整

--- src/Td.Kylin.Entity/Circle/Circle_Post.cs
+++ src/Td.Kylin.Entity/Circle/Circle_Post.cs
@@ -10,66 +10,70 @@ namespace Td.Kylin.Entity
     /// </summary>
     [Table("Circle_Post")]
     public class Circle_Post : BaseEntity
-    {		
-		///<summary>
-		///数据ID
-		///</summary>
-		public long PostID{get;set;}
-				
-		///<summary>
-		///主题ID
-		///</summary>
-		public long TopicID{get;set;}
-				
-		///<summary>
-		///用户ID
-		///</summary>
-		public long UserID{get;set;}
+    {
+        ///<summary>
+        ///数据ID
+        ///</summary>
+        public long PostID { get; set; }
+
+        ///<summary>
+        ///主题ID
+        ///</summary>
+        public long TopicID { get; set; }
+        /// <summary>
+        /// 评论父ID
+        /// </summary>
+        public long ParentUserID { get; set; }
+        ///<summary>
+        ///用户ID
+        ///</summary>
+        public long UserID { get; set; }
 
         ///<summary>
         ///用户昵称
         ///</summary>
         [Column(TypeName = "nvarchar(20)")]
-        public string Username{get;set;}
+        public string Username { get; set; }
 
         ///<summary>
         ///帖子标题，回帖无标题
         ///</summary>
         [Column(TypeName = "nvarchar(50)")]
-        public string Title{get;set;}
+        public string Title { get; set; }
 
         ///<summary>
         ///内容
         ///</summary>
         [Column(TypeName = "text")]
-        public string Content{get;set;}
+        public string Content { get; set; }
 
         ///<summary>
         ///帖子图片
         ///</summary>
         [Column(TypeName = "nvarchar(500)")]
-        public string Pics{get;set;}
-				
-		///<summary>
-		///评论数
-		///</summary>
-		public int Reviews{get;set;}
-				
-		///<summary>
-		///点赞支持
-		///</summary>
-		public int Support{get;set;}
-				
-		///<summary>
-		///不喜欢、踩
-		///</summary>
-		public int Dislike{get;set;}
+        public string Pics { get; set; }
+
+        ///<summary>
+        ///评论数
+        ///</summary>
+        public int Reviews { get; set; }
+
+        ///<summary>
+        ///点赞支持
+        ///</summary>
+        public int Support { get; set; }
+
+        ///<summary>
+        ///不喜欢、踩
+        ///</summary>
+        public int Dislike { get; set; }
 
         ///<summary>
         ///数据建立时间
         ///</summary>
-        
-        [Column(TypeName = "datetime")]public DateTime CreateTime{get;set;}
-				
-	}
+
+        [Column(TypeName = "datetime")]
+        public DateTime CreateTime { get; set; }
+
+    }
 }
---
+++ src/Td.Kylin.SQLCodefirst/Migrations/20160909092455_CircleAdjust.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Td.Kylin.SQLCodefirst.Migrations
+{
+    public partial class CircleAdjust : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "ParentUserID",
+                table: "Circle_Post",
+                nullable: false,
+                defaultValue: 0L);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ParentUserID",
+                table: "Circle_Post");
+        }
+    }
+}
