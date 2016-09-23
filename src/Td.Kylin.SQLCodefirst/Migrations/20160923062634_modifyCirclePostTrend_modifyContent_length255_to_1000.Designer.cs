1d0dd9048f3997de4703ac22c9c4ba43f40a403e
伍朝辉 <337883612@qq.com>
2016-09-23 14:31:28 +0800

    更新回复内容属性由nvarchar(255)调整为nvarchar(1000)

--- src/Td.Kylin.SQLCodefirst/Migrations/20160923062634_modifyCirclePostTrend_modifyContent_length255_to_1000.Designer.cs
+++ src/Td.Kylin.SQLCodefirst/Migrations/20160923062634_modifyCirclePostTrend_modifyContent_length255_to_1000.Designer.cs
@@ -8,9 +8,10 @@ using Td.Kylin.SQLCodeFirst;
 namespace Td.Kylin.SQLCodefirst.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20160923062634_modifyCirclePostTrend_modifyContent_length255_to_1000")]
+    partial class modifyCirclePostTrend_modifyContent_length255_to_1000
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.0.0-rtm-21431")
