fd85af7c5d4226dac0d859d4e705040b6adef89f
yk <yk@DESKTOP-97CR00P>
2016-10-11 14:38:43 +0800

    新增用户充值订单

---
+++ src/Td.Kylin.Entity/User/User_RechargeOrder.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Td.Kylin.Entity.User
+{
+    /// <summary>
+    /// 用户充值订单
+    /// </summary>
+    public class User_RechargeOrder
+    {
+        /// <summary>
+        /// 订单ID
+        /// </summary>
+        public long OrderID { get; set; }
+        /// <summary>
+        /// 订单编号
+        /// </summary>
+        public string OrderCode { get; set; }
+        /// <summary>
+        /// 充值金额
+        /// </summary>
+        public decimal Amount { get; set; }
+        /// <summary>
+        /// 用户名称
+        /// </summary>
+        public string UserName { get; set; }
+        /// <summary>
+        /// 用户ID
+        /// </summary>
+        public long UserID { get; set; }
+        /// <summary>
+        /// 充值状态(枚举：UserRechargeStatus)
+        /// </summary>
+        public int Status { get; set; }
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime CreateTime { get; set; }
+        /// <summary>
+        /// 充值完成时间
+        /// </summary>
+        public DateTime? CompleteTime { get; set; }
+        /// <summary>
+        /// 拓展数据
+        /// </summary>
+        public string ExpandData { get; set; }
+    }
+}
---
+++ src/Td.Kylin.EnumLibrary/User/UserRechargeStatus.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Td.Kylin.EnumLibrary.User
+{
+    /// <summary>
+    /// 用户充值状态
+    /// </summary>
+    public enum UserRechargeStatus
+    {
+        /// <summary>
+        /// 充值中
+        /// </summary>
+        Rechargeing = 1,
+        /// <summary>
+        /// 充值成功
+        /// </summary>
+        Success = 2,
+        /// <summary>
+        /// 充值失败
+        /// </summary>
+        Fail = 3
+    }
+}
