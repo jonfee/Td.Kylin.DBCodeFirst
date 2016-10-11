6f8c25ebb4b326d065129d69d9a5aaef8f4feada
yk <yk@DESKTOP-97CR00P>
2016-10-11 15:04:12 +0800

    统一命名空间

--- src/Td.Kylin.Entity/User/User_RechargeOrder.cs
+++ src/Td.Kylin.Entity/User/User_RechargeOrder.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Td.Kylin.Entity.User
+namespace Td.Kylin.Entity
 {
     /// <summary>
     /// 用户充值订单
